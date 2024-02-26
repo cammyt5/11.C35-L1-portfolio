212f1af97ab5168324aa0d79f60f9b87cfca42b2
Cameron Dougal <cdougal@mit.edu>
2024-02-26 13:30:44 -0500

    lab 3 step 2

--- global.js
+++ global.js
@@ -5,10 +5,5 @@ function $$ (selector, context = document) {
 }
 
 let navLinks = $$("nav a")
-console.log(navLinks)
 let currentLink = navLinks.find(a => a.host === location.host && a.pathname === location.pathname)
-
-for (let a of navLinks) {
-    console.log(a.host, location.host, a.pathname, location.pathname)
-}
 currentLink?.classList.add("current");
